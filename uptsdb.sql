de124d0999427b971ea6eed502c28f31fda32f52
tanjie <tanjie@unionpayintl.com>
2019-01-08 19:47:07 +0800

    update xmind

--- uptsdb.sql
+++ uptsdb.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50617
 File Encoding         : 65001
 
-Date: 2018-12-25 09:03:11
+Date: 2019-01-07 18:08:22
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -37,6 +37,45 @@ CREATE TABLE `upts_candle_info` (
 -- ----------------------------
 
 -- ----------------------------
+-- Table structure for `upts_param_info`
+-- ----------------------------
+DROP TABLE IF EXISTS `upts_param_info`;
+CREATE TABLE `upts_param_info` (
+  `param_key` varchar(20) NOT NULL,
+  `param_value` varchar(20) DEFAULT NULL,
+  `param_desc` varchar(30) DEFAULT NULL,
+  `update_time` timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3),
+  PRIMARY KEY (`param_key`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of upts_param_info
+-- ----------------------------
+INSERT INTO `upts_param_info` VALUES ('buyQueueSize', '0', '买单队列深度', '2019-01-07 17:38:04.689');
+INSERT INTO `upts_param_info` VALUES ('forceOrderNum', '0', '历史爆仓单数', '2019-01-07 17:38:05.702');
+INSERT INTO `upts_param_info` VALUES ('queueSizeMax', '0', '总队列深度', '2019-01-07 17:38:06.386');
+INSERT INTO `upts_param_info` VALUES ('sellQueueSize', '0', '卖单队列深度', '2019-01-06 15:52:49.437');
+
+-- ----------------------------
+-- Table structure for `upts_report_info`
+-- ----------------------------
+DROP TABLE IF EXISTS `upts_report_info`;
+CREATE TABLE `upts_report_info` (
+  `id` varchar(20) NOT NULL DEFAULT '',
+  `force_order_num` varchar(10) DEFAULT NULL,
+  `force_order_cost` decimal(20,0) DEFAULT NULL,
+  `profit` decimal(20,0) DEFAULT NULL,
+  `sprofit` decimal(20,0) DEFAULT NULL,
+  `scost` decimal(20,0) DEFAULT NULL,
+  `update_time` timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of upts_report_info
+-- ----------------------------
+
+-- ----------------------------
 -- Table structure for `upts_trade_info`
 -- ----------------------------
 DROP TABLE IF EXISTS `upts_trade_info`;
@@ -50,7 +89,7 @@ CREATE TABLE `upts_trade_info` (
   `size` int(5) DEFAULT NULL,
   `high` double(30,3) DEFAULT NULL,
   `low` double(30,3) DEFAULT NULL,
-  `flag` varchar(1) COLLATE utf8_bin DEFAULT NULL COMMENT '0:建；1:平',
+  `flag` varchar(1) COLLATE utf8_bin DEFAULT NULL COMMENT '0:建；1:平；2:损单；3:损单平仓',
   `profit` decimal(14,3) DEFAULT NULL,
   `rang` decimal(14,3) DEFAULT NULL,
   `bsum` double(30,3) DEFAULT NULL,
@@ -64,28 +103,3 @@ CREATE TABLE `upts_trade_info` (
 -- ----------------------------
 -- Records of upts_trade_info
 -- ----------------------------
-
--- ----------------------------
--- Table structure for `upts_trade_info_his`
--- ----------------------------
-DROP TABLE IF EXISTS `upts_trade_info_his`;
-CREATE TABLE `upts_trade_info_his` (
-  `order_id` varchar(20) COLLATE utf8_bin NOT NULL,
-  `btime` varchar(20) COLLATE utf8_bin DEFAULT '',
-  `bprice` double(30,3) DEFAULT NULL,
-  `sprice` double(30,3) DEFAULT NULL,
-  `stime` varchar(20) COLLATE utf8_bin DEFAULT '',
-  `sider` varchar(20) COLLATE utf8_bin DEFAULT '' COMMENT 'buy,sell',
-  `size` int(5) DEFAULT NULL,
-  `high` double(30,3) DEFAULT NULL,
-  `low` double(30,3) DEFAULT NULL,
-  `flag` varchar(1) COLLATE utf8_bin DEFAULT NULL COMMENT '0:建；1:平',
-  `profit` decimal(14,3) DEFAULT NULL,
-  `rang` decimal(14,3) DEFAULT NULL,
-  `bsum` double(30,3) DEFAULT NULL,
-  `ssum` double(30,3) DEFAULT NULL,
-  `strategy` varchar(20) COLLATE utf8_bin DEFAULT NULL,
-  `create_time` timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-  `update_time` timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3),
-  PRIMARY KEY (`order_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
